c757659c45d73fdeac11f8ad9b1c3521391da255
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-11-03 17:30:18 +0300

    debug

--- src/KernelMatrixAdd.cu
+++ src/KernelMatrixAdd.cu
@@ -1,4 +1,5 @@
 #include <KernelMatrixAdd.cuh>
+#include <cassert>
 
 __global__ void KernelMatrixAdd(int height, int width, size_t pA, size_t pB, size_t pC, float* A, float* B, float* result) {
   int row = blockIdx.x * blockDim.x + threadIdx.x;
@@ -12,6 +13,8 @@ __global__ void KernelMatrixAdd(int height, int width, size_t pA, size_t pB, siz
     float* rowC = (float*)((char*)result + i * pC);
 
     for(int j = col; j < width; j += stride_col) {
+      assert(rowA[j] == 1.0f);
+      assert(rowB[j] == 2.0f);
        rowC[j] = 3.0f;//rowA[j] + rowB[j];
     }
 	}
