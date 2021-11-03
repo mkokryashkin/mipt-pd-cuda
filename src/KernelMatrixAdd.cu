56ba266d8b6aadad5437f23cfaeb0817e7916362
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-11-03 18:31:17 +0300

    remove assertions

--- src/KernelMatrixAdd.cu
+++ src/KernelMatrixAdd.cu
@@ -1,8 +1,6 @@
 #include <KernelMatrixAdd.cuh>
-#include <assert.h>
 
 __global__ void KernelMatrixAdd(int height, int width, size_t pA, size_t pB, size_t pC, float* A, float* B, float* result) {
-  assert(false);
   int row = blockIdx.x * blockDim.x + threadIdx.x;
   int col = blockIdx.y * blockDim.y + threadIdx.y;
 	int stride_row = blockDim.x * gridDim.x;
@@ -14,11 +12,7 @@ __global__ void KernelMatrixAdd(int height, int width, size_t pA, size_t pB, siz
     float* rowC = (float*)((char*)result + i * pC);
 
     for(int j = col; j < width; j += stride_col) {
-      assert(rowA[j] == 1.0f);
-      assert(rowB[j] == 2.0f);
-      assert(rowC[j] == 0.0f);
-      rowC[j] = 3.0f;//rowA[j] + rowB[j];
-      assert(rowC[j] = 3.0f);
+      rowC[j] = rowA[j] + rowB[j];
     }
 	}
 }
