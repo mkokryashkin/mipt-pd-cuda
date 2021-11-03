32625e827c3100fe5adf5e482d25c60b2466eb7a
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-11-03 22:28:24 +0300

    cosine

--- runners/06-cosine-vector.cu
+++ runners/06-cosine-vector.cu
@@ -1,7 +1,24 @@
 #include <CosineVector.cuh>
-
+#include <stdio.h>
+#include <assert.h>
 
 int main() {
+  const int vec_size = 1 << 20;
+  float *vec1 = new float[vec_size]();
+  float *vec2 = new float[vec_size]();
+
+  vec1[0] = 1.0f;
+  vec2[0] = 1.0f;
+  vec2[1] = 1.0f;
+
+  float result = CosineVector(vec_size, vec1, vec2);
+  printf("%f\n", result);
+  fflush(stdout);
+  //assert(result == 2.0f * (1 << 20));
+
+  result = 0  delete[] vec1;
+  delete[] vec2;
+  return 0;
 
 
 }
--- src/CosineVector.cu
+++ src/CosineVector.cu
@@ -1,6 +1,11 @@
 #include <CosineVector.cuh>
+#include <ScalarMulRunner.cuh>
+#include <cmath>
 
 float CosineVector(int numElements, float* vector1, float* vector2) {
-  return 0.0f;
+  float module1 = sqrt(ScalarMulTwoReductions(numElements, vector1, vector1, 1024));
+  float module2 = sqrt(ScalarMulTwoReductions(numElements, vector2, vector2, 1024));
+  float scalar = ScalarMulTwoReductions(numElements, vector1, vector2, 1024);
+  return scalar / (module1 * module2);
 }
 
